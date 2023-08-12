96d226583eeb486b3f4b53206129bfc0fd2f6c4a
laleguliyeva <lale.gulieva1996@gmail.com>
2023-08-12 12:34:30 +0400

    header added

---
+++ task1/app.js
@@ -0,0 +1,43 @@
+// let ad = "Mikayil"
+
+// alert("hey,there! I'm using JS")
+// alert("Welcome,dear")
+// alert("HI, I am "+  ad )
+// alert(ad +" has a car"  + ad + "loves his car ")
+// alert(ad +" is a good boy")
+// let username = "Lale"
+// prompt("adinizi daxil edin:")
+// let userName = confirm ('are you ready to learn JavaScript?')
+// let ad = prompt('adinizi daxil edin')
+// let soyad = prompt('soyadinizi daxil edin')
+
+// let name = "John"
+// let admin = name
+// console.log(admin)
+// let age= prompt('How old are you',18)
+// console.log(age);
+
+// let browser=prompt("Enter your browser")
+// if(browser==="Edge"){
+//     console.log("You've got the Edge")
+
+// }
+// let a = +prompt("Input the first integer")
+// let b = +prompt("Input the second integer")
+// operator = prompt("Input operator")
+// let sum;
+// switch(operator){
+//     case "+":
+//     sum= a+b;
+//     break
+//     case "-":
+//     sum= a-b;
+//     break
+//     case "*":
+//     sum =a*b;
+//     break
+//     case "/":
+//     sum =a/b;
+
+// }
+// console.log(sum)
