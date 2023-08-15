6c6251de776f58be609ef9fe4be15149b91ebf68
laleguliyeva <lale.gulieva1996@gmail.com>
2023-08-15 12:20:24 +0400

    for created

--- task1/app.js
+++ task1/app.js
@@ -41,3 +41,44 @@
 
 // }
 // console.log(sum)
+// if(0) {console.log('beli, duz deyirsen')}
+// else{
+//     console.log('xeyr dostum,seni aldadiblar')
+// }
+// let eded = prompt('ededi daxil edin, cut/tek oldugunu yoxlayaq ')
+// if(eded %2 === 0) {
+//     console.log(`${eded} ededi cut ededdir`)
+// }
+// else{ 
+//     console.log(`${eded} ededi tek ededdir`)
+// }
+// let ad = prompt('adinizi daxil edin:')
+// let cins = prompt(' cinsinizi daxil edin:')
+// if (cins=== "K") {
+//     console.log(`hey, mr, ${ad}`)
+// }
+// else if (cins=== 'Q') {
+//     console.log(`hey, ms ${ad}`)
+// }
+// else{
+//     console.log('gender is not found')
+// }
+
+// let eded= prompt('eded daxil edin')
+//  if(eded % 3 ===0 && eded % 5 === 0) {
+//     console.log('fizzbuzz')
+// }
+// else if (eded % 3 === 0) {
+//     console.log('fizz')
+// }
+// else if (eded % 5 ===0) {
+//     console.log('buzz')
+// }
+
+let sum = 0;
+for (let i = 0; i < 100; i++) {
+    if (i % 2 === 1) {
+        sum = sum + i;
+    }
+}
+console.log(sum)
